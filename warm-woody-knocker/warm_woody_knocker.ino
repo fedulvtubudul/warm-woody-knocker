27c428ec32e5f3fe127b12a710d3a13d75906fcb
Alexander Gorbunov <fedulvtubudul@gmail.com>
2020-01-02 00:07:39 +0700

    Remove some outdated unused code

--- warm-woody-knocker/warm_woody_knocker.ino
+++ warm-woody-knocker/warm_woody_knocker.ino
@@ -16,19 +16,8 @@
 #include "src/Player/Player.h"
 
 
-//#include <U8g2lib.h>
-//
-//#define U8X8_HAVE_HW_I2C
-//#ifdef U8X8_HAVE_HW_I2C
-//  #include <Wire.h>
-//#endif
-
-
-#define LED_OUTPUT LED_BUILTIN
 #define LCD_LINE_WIDTH 16
 #define LCD_LINES_COUNT 2
-#define ONBEAT_OUTPUT 8
-#define OFFBEAT_OUTPUT 9
 
 
 Encoder *encoder;
@@ -44,9 +33,6 @@ uint32_t const buttonHoldThreshold = 2000;
 #define RHYTHMS_COUNT 2
 Rhythm *rhythms[RHYTHMS_COUNT];
 int currentRhythmIndex;
-
-#define MIN_TEMPO 15
-#define MAX_TEMPO 300
 
 #define BPM_TO_MICRO 60000000lu
 
@@ -77,36 +63,6 @@ Parameter *currentParameter();
 
 Player *player;
 
-enum AccentMode {
-    accentMode0,
-    accentMode1,
-    accentMode2,
-    accentMode3,
-    accentMode4,
-    accentMode5,
-    accentMode6,
-    accentMode7,
-    accentMode8,
-    accentMode9,
-    accentMode10,
-    accentMode11,
-    accentMode12,
-    accentMode13,
-    accentMode14,
-    accentMode15,
-    accentMode16,
-	
-    accentModeClave32,
-    accentModeClave23,
-
-    accentModeMaxValue,
-  };
-int accentMode = accentMode4;
-
-
-
-void refreshBeepIntervals(void);
-void beepIfNeeded(void);
 
 uint8_t positionForPendulumState(uint8_t state);
 void movePendulumIfNeeded(unsigned long currentTime);
@@ -122,15 +78,8 @@ void printAccentValue(void);
 void clearPendulum(uint8_t prevPosition, uint8_t newPosition);
 void printPendulum(uint8_t prevPosition, uint8_t newPosition);
 
-//U8G2_SSD1306_128X64_VCOMH0_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
-
-
-int positionX = 0;
-int positionY = 0;
 
 void setup() {
-//	Serial.begin(115200);
-
 	setupPlayer();
 	setupRhythms();
 	setupGlobalParameters();
@@ -230,7 +179,6 @@ void movePendulumIfNeeded(unsigned long currentTime) {
     clearPendulum(pendulumPosition, neededPosition);
 	printPendulum(pendulumPosition, neededPosition);
 	pendulumPosition = neededPosition;
-//	u8g2.sendBuffer();
   }
 }
 
@@ -251,56 +199,36 @@ uint8_t positionForPendulumState(uint8_t state) {
 void clearPendulum(uint8_t prevPosition, uint8_t newPosition) {
   // -->
   if (prevPosition < newPosition) {
-	  #if 1
 	  lcd.setCursor(prevPosition, PENDULUM_LINE);
 	  for (uint8_t i = prevPosition; i < newPosition; ++i) {
 	    lcd.print((char)32);
 	  }
-	  #else
-		  u8g2.setDrawColor(0);
-		  u8g2.drawBox(prevPosition, 0, newPosition-prevPosition, 16);
-	  #endif
   }
 	
   // <--
   else if (prevPosition > newPosition) {
-	  #if 1
 	  lcd.setCursor(newPosition + PENDULUM_WIDTH, PENDULUM_LINE);
 	  for (uint8_t i = newPosition; i < prevPosition; ++i) {
 	    lcd.print((char)32);
 	  }
-	  #else
-		  u8g2.setDrawColor(0);
-		  u8g2.drawBox(newPosition+PENDULUM_WIDTH, 0, prevPosition-newPosition, 16);
-	  #endif
   }
 }
 
 void printPendulum(uint8_t prevPosition, uint8_t newPosition) {
   // -->
   if (prevPosition < newPosition) {
-	  #if 1
 	  lcd.setCursor(prevPosition + PENDULUM_WIDTH, PENDULUM_LINE);
 	  for (uint8_t i = prevPosition; i < newPosition; ++i) {
 	    lcd.print((char)255);
 	  }
-	  #else
-		  u8g2.setDrawColor(1);
-		  u8g2.drawBox(newPosition, 0, PENDULUM_WIDTH, 16);
-	  #endif
   }
 	
   // <--
   else if (prevPosition > newPosition) {
-	  #if 1
 	  lcd.setCursor(newPosition, PENDULUM_LINE);
 	  for (uint8_t i = newPosition; i < prevPosition; ++i) {
 	    lcd.print((char)255);
 	  }
-	  #else
-		  u8g2.setDrawColor(1);
-		  u8g2.drawBox(newPosition, 0, PENDULUM_WIDTH, 16);
-	  #endif
   }
 }
 
@@ -348,19 +276,6 @@ void buttonClickAction(void) {
 	currentParameterIndex = (currentParameterIndex + 1) % parametersCount;
 
 	modeChanged = true;
-	
-	Rhythm *r = rhythms[currentRhythmIndex];
-	int rpc = r->getParametersCount();
-
-
-	Serial.println(parametersCount);
-	
-	Serial.print("rpc = ");
-	Serial.println(rpc);
-
-	Serial.println(currentParameterIndex);
-
-
 }
 
 void buttonHoldAction(void) {
