0322cc7c23941b96ed582d15a4ecc254bd433dff
Alexander Gorbunov <fedulvtubudul@gmail.com>
2021-05-30 12:37:34 +0700

    Optimise saving eeprom data (save only changed values and only by a special trigger)

--- warm-woody-knocker/src/Storage/StoredParameter.h
+++ warm-woody-knocker/src/Storage/StoredParameter.h
@@ -4,19 +4,9 @@
 #include "Arduino.h"
 
 
-
-// EEPROM memory layout:
-// 0x0000: Scheme version (uint16_t)
-// 0x0002: Rhythm (uint8_t)
-// 0x0003: Volume (uint16_t)
-// 0x0005: Tempo (uint16_t)
-// 0x0007: Meter (uint8_t)
-// 0x0008: Division (uint8_t)
-// 0x0009: Pattern (uint16_t)
-
 enum StoredParameter : int {
-	storedParameterScheme,
-	storedParameterRhythm,
+    storedParameterScheme,
+    storedParameterRhythm,
     storedParameterVolume,
     storedParameterTempo,
     storedParameterMeter,
